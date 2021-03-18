99d03d7a03f22bb921ac32765dd50faf96b7ddbd
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-18 00:48:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,9 +28,10 @@ class Transfer
 
   def reverse_transfer
     if @status = "complete"
-    sender.balance += amount
-    receiver.balance -= amount
-    @status = "reversed"
+      sender.balance += amount
+      receiver.balance -= amount
+      @status = "reversed"
+    end
   end
 
 end
