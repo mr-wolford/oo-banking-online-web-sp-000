b3732e4ce6ccbf5aa0a84e3b398485720957dfba
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-18 00:37:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,6 @@ class Transfer
       receiver.balance += amount
       sender.balance -= amount
       @status = "complete"
-      binding.pry
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
