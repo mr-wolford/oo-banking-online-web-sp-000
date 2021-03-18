35c8ec593d084a5effc507c77e90e356474c8749
Matt Wolford <matthew.r.wolford@gmail.com>
2021-03-18 00:41:04 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,10 +15,12 @@ class Transfer
   end
 
   def execute_transaction
-    if self.valid? && @status == "pending"
-      receiver.balance += amount
-      sender.balance -= amount
-      @status = "complete"
+    if self.valid? && 
+      @status == "pending" 
+      && amount < sender.balance
+        receiver.balance += amount
+        sender.balance -= amount
+        @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
